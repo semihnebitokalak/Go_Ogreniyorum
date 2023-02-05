c83cd45c8f150cae6a5201ebd79163128f987bd6
Semih Nebi Tokalak <semihnebi54@gmail.com>
2023-02-05 13:50:03 +0300

    toplama fonksiyonu eklendi

--- main.go
+++ main.go
@@ -5,5 +5,14 @@ import "fmt"
 func main() {
 
 	fmt.Println("Hello, world!")
+	Toplama()
+}
+func Toplama() {
+
+	var sayi1, sayi2 int
+	fmt.Scan(&sayi1)
+	fmt.Scan(&sayi2)
+
+	fmt.Println(sayi1 + sayi2)
 
 }
