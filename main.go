6338b56bbbcb2900bbffca4560b59cdb013ec64d
Semih Nebi Tokalak <semihnebi54@gmail.com>
2023-02-05 13:57:50 +0300

    Çikartma fonksiyonu eklendi

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ func main() {
 
 	fmt.Println("Hello, world!")
 	Toplama()
+	Çikartma()
 }
 func Toplama() {
 
@@ -13,6 +14,17 @@ func Toplama() {
 	fmt.Scan(&sayi1)
 	fmt.Scan(&sayi2)
 
-	fmt.Println(sayi1 + sayi2)
+	fmt.Println("toplamanin sonucu :", sayi1+sayi2)
+
+}
+
+func Çikartma() {
+
+	var sayi1, sayi2 int
+	fmt.Println("bir sayi giriniz")
+	fmt.Scan(&sayi1)
+	fmt.Println("bir sayi giriniz")
+	fmt.Scan(&sayi2)
+	fmt.Println("çikartmanin sonucu: ", sayi1-sayi2)
 
 }
