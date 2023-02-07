ff310b396c878b380ad63752f7ef2f79db10f158
Semih Nebi Tokalak <semihnebi54@gmail.com>
2023-02-07 13:53:43 +0300

    10 sayısının üssünü aldık

--- main.go
+++ main.go
@@ -1,17 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 
 	fmt.Println("Hello, world!")
 	Toplama()
 	Çikartma()
+	Üsalma()
 }
 func Toplama() {
 
 	var sayi1, sayi2 int
+	fmt.Println("toplama yapmak istediğiniz birinci sayiyi giriniz:")
 	fmt.Scan(&sayi1)
+	fmt.Println("toplama yapmak istediğiniz ikinci sayiyi giriniz:")
 	fmt.Scan(&sayi2)
 
 	fmt.Println("toplamanin sonucu :", sayi1+sayi2)
@@ -21,10 +27,14 @@ func Toplama() {
 func Çikartma() {
 
 	var sayi1, sayi2 int
-	fmt.Println("bir sayi giriniz")
+	fmt.Println("çikartma yapmak istediğiniz birinci sayiyi giriniz")
 	fmt.Scan(&sayi1)
-	fmt.Println("bir sayi giriniz")
+	fmt.Println("çikartma yapmak istediğiniz ikinci sayiyi giriniz")
 	fmt.Scan(&sayi2)
 	fmt.Println("çikartmanin sonucu: ", sayi1-sayi2)
 
+}
+func Üsalma() {
+	c := math.Pow10(2) //burda 10 sayisinin üssünü aldik.
+	fmt.Println(c)
 }
