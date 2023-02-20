ad167fe6f2650e1442869530d0033542cb54c4d7
Semih Nebi Tokalak <semihnebi54@gmail.com>
2023-02-20 11:55:58 +0300

    Hello World yazdık.

---
+++ 04_zero.values_multiple.variable/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var name, age, height, weight, isMarried = "Semih", 18, 196, 85, false
+
+	//name,age,height,weight,isMarried = "Semih",18,196,85,false
+
+	fmt.Println(name)
+	fmt.Println(age)
+	fmt.Println(height)
+	fmt.Println(weight)
+	fmt.Println(isMarried)
+
+	/*
+		     var name string
+			 fmt.Println(name)       bunun çıktısı bana hiç bir şey vermez.
+
+			 var age int
+			 fmt.Println(age)       bunun çıktısı bana 0 değerini verir.
+
+			 Note = string için zero value değeri boştur, integer için zero value değeri 0 dır,bool'un zero value değeri "false"dur.
+
+	*/
+
+}
